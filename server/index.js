2896114a7bc149293ad0c944743d036f5593fb7a
Adam <amkhadov2014@mail.ru>
2022-08-10 02:11:01 +0300

    routes

--- server/index.js
+++ server/index.js
@@ -2,19 +2,24 @@ require("dotenv").config()
 const sequelize = require("./db")
 const models = require("./models/models")
 const cors = require("cors")
+const router = require("./routes/index")
+
+
 const PORT = process.env.PORT || 5000
 
 const express  = require("express")
 
 
 
+
 const app = express()
 app.use(cors())
 app.use(express.json())
+app.use("/api",router)
 
-app.get("/", (req,res)=>{
-    res.status(200).json({message: "WORKING!!!"})
-})
+// app.get("/", (req,res)=>{
+//     res.status(200).json({message: "WORKING!!!"})
+// })
 
 
 const start = async ()=>{
